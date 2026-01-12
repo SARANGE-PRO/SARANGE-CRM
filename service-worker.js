33242584039f15124b7e1d7344a04c4c6a71b802
SARANGE-PRO <contact@sarange.fr>
2026-01-12 11:07:36 +0100

    Add files via upload
    Version 1

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'sarange-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/lucide-react@latest/dist/umd/lucide-react.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
